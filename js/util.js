da9dd6e00eb43e6acbf84901b7274cbcfb9f4feb
Marius Olbertz <marius.olbertz@gmail.com>
2016-09-14 16:32:33 +0200

    Improved file size. Renamed classes.

--- js/util.js
+++ js/util.js
@@ -1,1 +1,1 @@
-App.Util=(()=>{return{resultParser:a=>{try{return JSON.parse(a)}catch(b){return void 0,null}},dateParser:a=>{var b=e=>{return 10>e?`0${e}`:e},c=`${a.getFullYear()}/${b(a.getMonth()+1)}/${b(a.getDate())} ${b(a.getHours())}:${b(a.getMinutes())}`;return c}}})();
+App.Util=(()=>{return{resultParser:a=>{return JSON.parse(a)},dateParser:a=>{var b=e=>{return 10>e?`0${e}`:e},c=`${a.getFullYear()}/${b(a.getMonth()+1)}/${b(a.getDate())} ${b(a.getHours())}:${b(a.getMinutes())}`;return c}}})();
